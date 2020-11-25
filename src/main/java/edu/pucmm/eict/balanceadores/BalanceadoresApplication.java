7ca5b23c58037ab396f7249e79c9c26c9483c0dc
Rafael <rafaelrfq@gmail.com>
2020-11-24 22:22:04 -0500

    test sessions

--- src/main/java/edu/pucmm/eict/balanceadores/BalanceadoresApplication.java
+++ src/main/java/edu/pucmm/eict/balanceadores/BalanceadoresApplication.java
@@ -1,17 +1,10 @@
 package edu.pucmm.eict.balanceadores;
 
-import com.hazelcast.config.Config;
-import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.web.WebFilter;
 import edu.pucmm.eict.balanceadores.entities.security.User;
 import edu.pucmm.eict.balanceadores.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Properties;
 
 @SpringBootApplication
 public class BalanceadoresApplication {
@@ -29,20 +22,5 @@ public class BalanceadoresApplication {
             userRepository.save(user1);
             userRepository.save(user2);
         }
-    }
-
-    @Bean
-    public Config config() {
-        return new Config();
-    }
-
-    @Bean
-    public WebFilter webFilter(@Qualifier("hazelcastInstance") HazelcastInstance hazelcastInstance) {
-
-        Properties properties = new Properties();
-        properties.put("instance-name", hazelcastInstance.getName());
-        properties.put("sticky-session", "false");
-
-        return new WebFilter(properties);
     }
 }
